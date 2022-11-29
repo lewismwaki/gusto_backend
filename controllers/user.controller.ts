889c7ff88f6d4f50354400f97bf230893d9d8685
lewis <lewis@thoriumorganicfoods.com>
2022-11-29 13:58:46 +0300

    test

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -44,7 +44,7 @@ export const logInUserHandler = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: "Please enter email and password!" });
+    return res.status(400).json({ error: "Please enter email and password" });
   }
 
   const existingUser = await UserModel.findOne({ email });
